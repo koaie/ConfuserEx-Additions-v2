1f5a954a3cc3c85d68b8b2aebf01485858c2bb50
yourvul <50146875+yourvul@users.noreply.github.com>
2019-07-04 10:38:16 +0100

    Add files via upload

---
+++ AntiWatermark/Protection/AntiWatermarkProtection.cs
@@ -0,0 +1,42 @@
+﻿using Confuser.Core;
+using dnlib.DotNet;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Confuser.Protections {
+	public class AntiWatermarkProtection : Protection {
+		public override string Name => "Anti Watermark";
+		public override string Description => "Removes the ProtectedBy watermark to prevent Protector detection.";
+		public override string Author => "HoLLy ";
+		public override string Id => "anti watermark";
+		public override string FullId => "HoLLy.AntiWatermark";
+		public override ProtectionPreset Preset => ProtectionPreset.Normal;
+
+		protected override void Initialize(ConfuserContext context) { }
+
+		protected override void PopulatePipeline(ProtectionPipeline pipeline) {
+			//watermark is added in the inspection stage, this executes right after
+			pipeline.InsertPostStage(PipelineStage.Inspection, new AntiWatermarkPhase(this));
+		}
+
+		public class AntiWatermarkPhase : ProtectionPhase {
+			public override ProtectionTargets Targets => ProtectionTargets.Modules;
+			public override string Name => "ProtectedBy attribute removal";
+
+			public AntiWatermarkPhase(ConfuserComponent parent) : base(parent) { }
+			protected override void Execute(ConfuserContext context, ProtectionParameters parameters) {
+				foreach (var m in parameters.Targets.Cast<ModuleDef>().WithProgress(context.Logger)) {
+					//look for watermark and remove it
+					var attr = m.CustomAttributes.Find("ProtectedByAttribute");
+					if (attr != null) {
+						m.CustomAttributes.Remove(attr);
+						m.Types.Remove((TypeDef)attr.AttributeType);
+					}
+				}
+			}
+		}
+	}
+}
